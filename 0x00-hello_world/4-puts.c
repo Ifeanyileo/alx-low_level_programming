d96fafc955c4e9ba92e3827b3e7d08feeb895e0e
Ifeanyileo <112099026+Ifeanyileo@users.noreply.github.com>
2022-09-08 22:31:48 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always O (Success)
  */
 int main(void)
 {puts("/"Programming is like building a multilingual puzzle");
-	return (0);
+	return (O);
