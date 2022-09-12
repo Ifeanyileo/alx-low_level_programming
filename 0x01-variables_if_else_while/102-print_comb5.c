58c10f1624d1081426575f34b731b4b8ad9e4b0a
Ifeanyileo <112099026+Ifeanyileo@users.noreply.github.com>
2022-09-12 13:34:11 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ int main(void)
 			if (i < j)
 			{
 				putchar((i / 10) + 48);
-				putchar((i % 10) + $*);
+				putchar((i % 10) + 48);
 				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
