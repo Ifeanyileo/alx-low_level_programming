3c0ed41bc324d9f471669c7101f324680611aa91
Ifeanyileo <aguleo13@yahoo.com>
2022-09-12 05:10:30 -0700

    11. The success combination in business is

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using the main function
+ * this "prints all possible different combination of 3 digit
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 0;
+	int b;
+	int d;
+	int f;
+	
+	while (c <= 999)
+	{
+		b = (c / 100 + '0');
+		d = (c / 10 % 10 + '0');
+		f = (c % 10 + '0');
+		if ((b < d) && (d < f))
+		{
+			putchar(b);
+			putchar(d);
+			putchar(f);
+
+			if (c != 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
