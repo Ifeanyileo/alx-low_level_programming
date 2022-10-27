dbd1ab6f46e203dd1b8f3774aeb7bc87c5274d9c
Ifeanyileo <112099026+Ifeanyileo@users.noreply.github.com>
2022-10-27 21:00:11 +0100

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,2 +1,23 @@
 #include "main.h"
 
+/**
+*get_endianness - determines the endianness of a machine.
+*
+*Return: 0 big endian, 1 little endian.
+*/
+
+int get_endianness(void)
+{
+	unsigned int x = 0x7343211;
+
+	char *c = (char *)&x;
+
+	if (*c)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
